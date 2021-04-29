9ccd3734333e1d1a0ec275e06d139322c62856e6
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-04-29 11:29:00 -0400

    Completed exercises 1-6 of Chapter 3, and started exercise 7

---
+++ Ch. 3 Exercises/Exercise_3_3.py
@@ -0,0 +1,6 @@
+consoles = ["nintendo", "playstation", "xbox", "pc"]
+
+print(f"My first console was a {consoles[0].title()} system!")
+print(f"My favorite consoles are {consoles[1].title()}'s.")
+print(f"My brother's favorite consoles are {consoles[2].title()} system.")
+print(f"Now I play on my {consoles[3].upper()} gaming system!")
