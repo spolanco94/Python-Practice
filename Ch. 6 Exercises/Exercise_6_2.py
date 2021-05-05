4e14f94368d34da4abf9133570a6cb57be6a972c
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-04 22:13:40 -0400

    Completed exercise 6-2

---
+++ Ch. 6 Exercises/Exercise_6_2.py
@@ -0,0 +1,11 @@
+##### Use a dictionary to store at least 5 people's names and favorite numbers
+favorite_numbers = {
+    "anna" : 22,
+    "jasmine" : 12,
+    "olivia" : 5, 
+    "farjan" : 67,
+    "tareen" : 102,
+}
+
+for val in favorite_numbers:
+    print(f"{val.title()}: {favorite_numbers[val]}")
