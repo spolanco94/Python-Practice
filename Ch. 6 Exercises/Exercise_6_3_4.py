8e80f030be566be81702cd227ddec8bf6d336878
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-04 22:31:03 -0400

    Completed exercise 6-3/6-4

---
+++ Ch. 6 Exercises/Exercise_6_3_4.py
@@ -0,0 +1,12 @@
+##### Store 5 programming related terms in a dictionary with their definitions 
+##### as values, then print each one neatly formatted
+terms = {
+    "conditionals" : "expressions used to handle decisions made by program",
+    "loops" : "repeating through sections of code until a certain goal is met",
+    "variables" : "storage containing various data and affixed with a label",
+    "lists" : "data type used to store multiple values in a single variable",
+    "dictionary" : "data type storing various key-value pairs",
+}
+
+for val in terms:
+    print(f"{val.title()}: {terms[val].capitalize()}\n")
