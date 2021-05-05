0c11a71ea03bd9dcac619d287989a37d524cad77
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-04 22:10:08 -0400

    Completed exercise 6-1

---
+++ Ch. 6 Exercises/Exercise_6_1.py
@@ -0,0 +1,13 @@
+##### Use a dictionary to store information about a person 
+
+farjan_profile = {
+    "first_name" : "farjan",
+    "last_name" : "khan",
+    "age" : 26,
+    "city" : "queens",
+}
+for val in farjan_profile:
+    if isinstance(farjan_profile[val], int):
+        print(f"{val} : {farjan_profile[val]}")
+    else:
+        print(f"{val} : {farjan_profile[val].title()}")
