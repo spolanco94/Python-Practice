7624ba538220d1304c710730aba742790e719110
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-11 22:21:53 -0400

    Completed Exercise 7-1

---
+++ Ch. 7 Exercises/Exercise_7_1.py
@@ -0,0 +1,12 @@
+##### Write a program asking user for their selection of a rental car
+car = input("What car would you like to rent today? ")
+
+if car[-1] == "s":
+    print(f"Let me see if we have any {car.title()}' in stock today. One moment" 
+    "please.")
+elif car == "bmw" :
+    print(f"Let me see if we have any {car.upper()}'s in stock today. One moment" 
+    "please.")
+else :
+    print(f"Let me see if we have any {car.title()}'s in stock today. One moment" 
+        "please.")
