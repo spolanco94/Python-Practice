7825f10365e32561e7ab710b2cc987a39a93b759
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-11 23:21:01 -0400

    Changed while loop to check for flag variable 'responded', and used an if/else block to verify that user input was 'y' or 'n'

--- Ch. 7 Exercises/Exercise_7_2.py
+++ Ch. 7 Exercises/Exercise_7_2.py
@@ -7,10 +7,14 @@ if party_size > 8 :
     print(f"For a party of {party_size}, there will be a 30 minute waiting "
     "period.")
     response = input("Will that be alright? (y/n) ")
-    
-    while response != "y" or response != "n" :
-        response = input("I am sorry, I do not understand your response. Please "
-        "respond with 'y' for yes or 'n' for no. ")
+    responded = False
+
+    while not responded:
+        if response.lower() == 'y' or response.lower() == 'n':
+            responded = True
+        else:
+            response = input("I am sorry, I do not understand your response. "
+            "Please respond with 'y' for yes or 'n' for no. ")
 
     if response.lower() == "y":
         print("Thank you for your patience. We will notify you once your table "
