1285c8cf90fa9b93f50fc03fad57d1f10b5a8988
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-13 12:24:46 -0400

    Completed Exercuse 8-7

---
+++ Ch. 8 Exercises/Exercise_8_7.py
@@ -0,0 +1,28 @@
+# Write a function called make_album() that builds a dictionary descirbing an
+# album. Take in an artist name, title, and make 3 dictionaries representing 
+# different albums. Use 'None' to add an optional parameter for the # of songs
+
+def make_album(title, artist, year, status, songs= None):
+    """Creates a dictionary of an album"""
+    
+    album = {
+        "title" : title,
+        "artist" : artist,
+        "year" : year,
+        "status" : status,
+    }
+    if songs == None:
+        album.setdefault("tracks", "N/A")
+
+    return album
+
+kod = make_album(title= "KOD", artist= "J. Cole", year= "2018", status= 
+"platinum")
+yhlqmdlg = make_album(title= "YHLQMDLG", artist= "Bad Bunny", year= "2020", 
+status= "Gold", songs= "20")
+blonde = make_album(title= "Blonde", artist= "Frank Ocean", year= "2016", 
+status= "Platinum", songs= "17")
+
+print(kod)
+print(yhlqmdlg)
+print(blonde)
