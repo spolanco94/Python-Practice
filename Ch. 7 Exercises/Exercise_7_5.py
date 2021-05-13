3864659de2f17d0055efda19b9635aee9c3a6086
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-12 23:42:36 -0400

    Completed exercise 7-5

---
+++ Ch. 7 Exercises/Exercise_7_5.py
@@ -0,0 +1,18 @@
+# Write a loop that ask users for their age and informs them of theater ticket 
+# costs. < 3 = free, 3 - 12 = $10, 12 and over = $15
+age = None
+
+while age == None:
+    print(f"Welcome to Awesome Movie Theaters. Tickets are free for anyone"
+    ", $10 for anyone ages 3 to 12 and $15 for anyone over 12. ")
+    age = input("What is your age? ")
+    age = int(age)
+    
+    if age >= 12:
+        print("That will be $15 for your ticket.")
+    elif age >= 3:
+        print("That will be $10 for your ticket.")
+    else:
+        print("There will be no fee for your ticket.")
+
+print("Please enjoy your film!")
