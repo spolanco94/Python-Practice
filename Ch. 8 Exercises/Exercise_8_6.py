261191c4ea2608de2a6401f2a546bcfabe7b11bf
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-13 11:50:02 -0400

    Completed Exercise 8-6

---
+++ Ch. 8 Exercises/Exercise_8_6.py
@@ -0,0 +1,11 @@
+# Write a function that takes in the name of a city and country and formats them
+# as "Santiago, Chile". 
+
+def format_city(city, country):
+    """Formats a city and country as 'City', 'Country'"""
+    formatted = f"{city.title()}, {country.title()}"
+
+    return formatted
+
+location = format_city(city= "paris", country= "france")
+print(location)
