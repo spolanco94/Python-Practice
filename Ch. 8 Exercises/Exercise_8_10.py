48e559b7406c178e1f580a10b03ab71e4dc9b4bc
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-14 01:46:16 -0400

    Completed Exercise 8-10

---
+++ Ch. 8 Exercises/Exercise_8_10.py
@@ -0,0 +1,21 @@
+# Expand the code from 8-9 to move each message to a new list called 
+# 'sent_messages'. Print both lists to make sure everything moved correctly.
+# Make a list of a series of text messages, and a function called 
+# 'show_messages()' to output them.
+
+def show_messages(messages, sent_messages):
+    """Displays a series of text messages"""
+    while messages:
+        curr = messages.pop()
+        print(f"> {curr}")
+        sent_messages.append(curr)
+
+    sent_messages.reverse()
+    print(f"Messages: {messages}")
+    print(f"Sent messages: {sent_messages}")
+    
+sent_messages = []
+messages = ["hey when is the party???", "i think i might sleep instead", 
+"its at 11!", "you promised you'd come!", "oh dang, fine!"]
+
+show_messages(messages, sent_messages)
