3ee01a2ad7e1d6bd4193eeece2ad1f4167d964c4
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-15 02:42:58 -0400

    Added module for Exercise 8-15

---
+++ Ch. 8 Exercises/Exercise_8_15 - printing_models.py
@@ -0,0 +1,7 @@
+# Add the functions for the example code 'printing_models.py' into a separate 
+# file called 'printing_functions.py'. Add an import statement to the top of 
+# 'printing_models.py' and call the functions.
+import 8_15_printing_functions
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
