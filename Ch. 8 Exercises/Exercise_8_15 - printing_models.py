a8ecce666266dd576545958bb6e2a4721a2c453d
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-15 03:03:56 -0400

    Updated import statement to function with pylance

--- Ch. 8 Exercises/Exercise_8_15 - printing_models.py
+++ Ch. 8 Exercises/Exercise_8_15 - printing_models.py
@@ -1,7 +1,7 @@
 # Add the functions for the example code 'printing_models.py' into a separate 
 # file called 'printing_functions.py'. Add an import statement to the top of 
 # 'printing_models.py' and call the functions.
-import printing_functions
+import printing_functions 
 
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
 completed_models = []
