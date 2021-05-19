55763ac9fd697f3c6d7e8f89f169d857a4d60c0a
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-18 22:02:44 -0400

    Complete Exercise 9-1, creating an instance from the Restaurant class and using its two methods.

---
+++ Ch. 9 Exercises/Exercise_9-1.py
@@ -0,0 +1,8 @@
+# Create an instance of the Restaraunt class. Print the two attributes, then 
+# call each method
+from Restaurant import Restaurant
+
+my_restaurant = Restaurant("El Amor", "Latin")
+print(f"{my_restaurant.name.title()}, {my_restaurant.cuisine.title()}")
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
