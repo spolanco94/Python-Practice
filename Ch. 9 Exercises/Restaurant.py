3a07e40d3016a5d9dc442b85e79b3bb82c57b089
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-25 11:22:14 -0400

    Expand Restaurant class to include a child class IceCreamStand that is a subset modeling an Ice Cream Stand version of a restaurant. Include a unique 'flavors' attribute and a display_flavors() function to take in and print out a list of ice cream flavors.

--- Ch. 9 Exercises/Restaurant.py
+++ Ch. 9 Exercises/Restaurant.py
@@ -1,11 +1,13 @@
 # Create a 'Restaurant' class, including a 'restaurant_name' and 'cuisine_type'
 # attribute as well as two methods, 'describe_restaurant' to print the name and 
 # cuisine type, and 'open_restaurant' to print that the restaurant is open.
+import string
+
 class Restaurant:
     """A simple restaurant class."""
 
     def __init__(self, name, cuisine) -> None:
-        """Initialize name, cuisine and customers served attributes."""
+        """Initialize name, cuisine and customers served 4attributes."""
         self.name = name
         self.cuisine = cuisine
         self.number_served = 0
@@ -25,3 +27,31 @@ class Restaurant:
     def increment_number_served(self, number_served):
         """Increment the value of number_served."""
         self.number_served += number_served
+
+class IceCreamStand(Restaurant):
+    """A subset of the Restaurant class representing an Ice Cream Stand."""
+
+    def __init__(self, name, cuisine, flavors) -> None:
+        """
+        Initialize the attributes of the parent Restaurant class.
+        Initialize flavors attribute for Ice Cream Stand class.
+        """
+        super().__init__(name, cuisine)
+        self.flavors = flavors
+
+    def display_flavors(self):
+        """Displays the list of available ice cream flavors."""
+        read = []
+        curr = ""
+        finished = False
+        self.flavors.reverse()
+
+        print(f"Welcome to {string.capwords(self.name)}.\nCurrently, the "
+               "flavors we have available are:")
+        
+        while not finished:
+            curr = self.flavors.pop()
+            print(f"\t> {curr.title()}")
+            read.append(curr)
+            if(self.flavors == []):
+                finished = True
