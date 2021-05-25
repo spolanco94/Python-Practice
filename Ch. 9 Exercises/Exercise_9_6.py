daaff8c7ab78354ca03ec3939d305604e5c506fa
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-25 11:23:16 -0400

    Complete exercise 9_6; use the new IceCreamStand class to create an instance and use the new display_flavors() method.

---
+++ Ch. 9 Exercises/Exercise_9_6.py
@@ -0,0 +1,8 @@
+# Add an IceCeamStand class to the Restaurant.py file. Initialize it with an 
+# attribute called 'flavors' to take in a list of ice cream flavors and write a 
+# function to display these flavors
+from Restaurant import IceCreamStand
+
+shakeys = IceCreamStand("shakey's", "italian gelato", ["vanilla", "chocolate", 
+                        "strawberry", "mango", "coffee", "cookies n cream"])
+shakeys.display_flavors()
