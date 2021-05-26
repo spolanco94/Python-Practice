fab1f14635786b2b24255fb0ecef03ce26393516
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-25 20:04:19 -0400

    Complete exercise 9-7; Create an instance from the new Admin class and use the show_privileges() function to display a list of privileges.

---
+++ Ch. 9 Exercises/Exercise_9_7.py
@@ -0,0 +1,9 @@
+# Add an Admin class to the User.py file, Add a 'privileges' attribute that 
+# stores in a list of string directives such as "can post", "can delete". Write
+# a method called show_privileges() that lists the Administrators privileges
+from User import Admin
+
+hal = Admin("hal", "9000", "1/12/1992", "ai", "urbana, illinois", 
+            ["can delete posts", "can create posts", "can speak", "can pilot", 
+            "can play chess"])
+hal.show_privileges()
