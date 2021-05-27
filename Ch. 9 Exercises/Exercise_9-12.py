945f6e0187e51fbf5df3e909cad3cbba0ba948b7
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-27 02:27:26 -0400

    Complete Exercise 9-12; Test the new admin.py file to make sure it still works properly with the User class.

---
+++ Ch. 9 Exercises/Exercise_9-12.py
@@ -0,0 +1,8 @@
+# Refactor the User class so that the Privileges and Admin classes are in a 
+# separate module. Check to see that they still work properly.
+from admin import Admin
+
+grizzly = Admin("grizzly", "bear", "1/1/1900", "male", "the forest", 
+                ["can eat berries", "can roar", "can hibernate", "can fish"])
+
+grizzly.privileges.show_privileges()
