5e8d89a09ca5d63467bd26b3d4dd609b6d986ef5
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-27 02:07:13 -0400

    Complete Exercise 9-9; Use the ElectricCar class from electric_car.py to make a new instance of an electric car with the default battery size, then call the upgrade battery function on it and check to see if it properly changes the battery size.

---
+++ Ch. 9 Exercises/Exercise_9-9.py
@@ -0,0 +1,14 @@
+# Using the final version of electric_car.py, add a method to the Battery class 
+# called upgrade_battery(). This method should check the battery size and set 
+# the capacity to 100 if it isn't already. Make an electric car with a default 
+# battery size, call get_range() before and after upgrading the battery.
+from electric_car import ElectricCar
+
+my_mustang = ElectricCar("ford", "mustang mach-e", 2021)
+
+my_mustang.get_descriptive_name()
+my_mustang.battery.describe_battery()
+my_mustang.battery.get_range()
+my_mustang.battery.upgrade_battery()
+my_mustang.battery.describe_battery()
+my_mustang.battery.get_range()
