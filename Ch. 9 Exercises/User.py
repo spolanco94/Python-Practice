07d5ca343788973e1415e90a0587ed1381dd09e1
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-27 02:26:50 -0400

    Refactor User module to separate Admin and Privilege classes in a separate module.

--- Ch. 9 Exercises/User.py
+++ Ch. 9 Exercises/User.py
@@ -34,33 +34,3 @@ class User:
     def reset_login_attempts(self):
         """Resets the login attempt count back to 0 upon successful login."""
         self.login_attempts = 0
-
-class Privileges:
-    """A class representing a list of user privileges."""
-
-    def __init__(self, privileges=["can monitor", "can pilot"]) -> None:
-        self.privileges = privileges
-
-    def show_privileges(self):
-        """Displays Administrators full privileges."""
-        read = []
-        curr = ""
-        finished = False
-        self.privileges.reverse()
-        
-        print(f"Your full administrator privileges are as follows:")
-        
-        while not finished:
-            curr = self.privileges.pop()
-            print(f"\t> {curr.title()}")
-            read.append(curr)
-            if(self.privileges == []):
-                finished = True
-
-class Admin(User):
-    """A subset of the User class that represents an Administrator user."""
-
-    def __init__(self, f_name, l_name, birthday, 
-                 gender, city, privileges) -> None:
-        super().__init__(f_name, l_name, birthday, gender, city)
-        self.privileges = Privileges(privileges)
---
+++ Ch. 9 Exercises/admin.py
@@ -0,0 +1,31 @@
+from User import User
+
+class Privileges:
+    """A class representing a list of user privileges."""
+
+    def __init__(self, privileges=["can monitor", "can pilot"]) -> None:
+        self.privileges = privileges
+
+    def show_privileges(self):
+        """Displays Administrators full privileges."""
+        read = []
+        curr = ""
+        finished = False
+        self.privileges.reverse()
+        
+        print(f"Your full administrator privileges are as follows:")
+        
+        while not finished:
+            curr = self.privileges.pop()
+            print(f"\t> {curr.title()}")
+            read.append(curr)
+            if(self.privileges == []):
+                finished = True
+
+class Admin(User):
+    """A subset of the User class that represents an Administrator user."""
+
+    def __init__(self, f_name, l_name, birthday, 
+                 gender, city, privileges) -> None:
+        super().__init__(f_name, l_name, birthday, gender, city)
+        self.privileges = Privileges(privileges)
