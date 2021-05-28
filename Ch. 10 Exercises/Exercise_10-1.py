695b1740e3ddde04b4f5c6b061d6dd3351a04a51
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-05-27 23:51:41 -0400

    Complete Exercise 10-1; Create a file called learning_python.txt and type a few lines that start with "In python you can...", then open this file and print out the text by reading the entire file, reading line by line, and by storing into a list and then reading.

---
+++ Ch. 10 Exercises/Exercise_10-1.py
@@ -0,0 +1,23 @@
+# Create a file called 'learning_python.txt' that have a few lines that start 
+# with the phrase 'In Python you can...'. Write a program that reads the file 
+# and prints what you wrote three times; once by reading the entire file, once 
+# by looping over the file object, and once by storing the lines in a list and 
+# working with it outside the block.
+filepath = 'Ch. 10 Exercises\learning_python.txt'
+with open(filepath) as file_object:
+    contents = file_object.read()
+
+print(f"Read entire file:\n{contents}")
+print("\n".rstrip())
+
+with open(filepath) as file_object:
+    for line in file_object:
+        print(f"Read line by line:\n{line.rstrip()}")
+
+print("\n".rstrip())
+with open(filepath) as file_object:
+    python_string = ''
+    for line in file_object:
+        python_string += line
+
+print(f"Store and read:\n{python_string}")
