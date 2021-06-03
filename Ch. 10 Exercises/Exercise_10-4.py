946b92cc14f78c4870f548c1bf826c74d2ddbd05
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-06-03 00:21:29 -0400

    Complete Exercise 10-4; Write a while loop prompting users for their names, and print a greeting to them while writing them to a file named 'guest_book.txt'

---
+++ Ch. 10 Exercises/Exercise_10-4.py
@@ -0,0 +1,16 @@
+# Write a while loop that prompts users for their name. When they enter their 
+# name, print a greeting to the screen and add a line recording their visit in a 
+# file called 'guest_book.txt'. Make sure each entry appears in a new line
+
+filename = 'guest_book.txt'
+
+with open(filename, 'w') as file_object:
+    checking_in = True
+    while checking_in:
+        patient = input("Please check-in here with your full name, or press 'c'"
+                        " to cancel: ")
+        if patient.lower() == 'c':
+            checking_in = False
+        else:
+            file_object.write(f"Welcome to Barnabus Hospital, {patient}. The "
+                                "doctor will see you soon.\n\n")
