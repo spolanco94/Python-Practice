39ac4a4a461552cf352d7ff669cb2587359eb489
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-06-02 23:48:14 -0400

    Restructure VS Code repository, thereby having to rename the 'filename' variable in both Exercise 10-1 and 10-2

--- Ch. 10 Exercises/Exercise_10-2.py
+++ Ch. 10 Exercises/Exercise_10-2.py
@@ -1,7 +1,7 @@
 # Use the replace() method to replace any word in a string with a different 
 # word. Read each line from the 'learning_python.txt' file and replace the word 
 # 'Python' with C.
-filename = 'Ch. 10 Exercises\learning_python.txt'
+filename = 'learning_python.txt'
 with open(filename) as file_object:
     for line in file_object:
         revised = line.replace("Python", "C")
