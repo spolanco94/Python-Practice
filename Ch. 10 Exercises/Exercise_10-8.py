ea88456fd45306f3236301d10395cc3dbd37aca4
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-06-07 23:23:19 -0400

    Complete Exercise 10-8; Create two files, cats.txt and dogs.txt, that store 3 breeds of each animal and write a try-except block that will either print out the contents of each file, or print an error message if a FileNotFoundError is caught

---
+++ Ch. 10 Exercises/Exercise_10-8.py
@@ -0,0 +1,25 @@
+# Make two files, cats.txt and dogs.txt, and store at least 3 kinds of each into
+# each file. Write the code in a try-except block that will catch a FileNotFound
+# error, or print the contents of each file if there are none. 
+
+cats = ["lynx", "sphynx", "persian"]
+dogs = ["shiba", "shih tsu", "miniature pinscher"]
+
+file_1 = "cats.txt"
+file_2 = "dogs.txt"
+
+with open(file_1, 'w') as f:
+    for cat in cats:
+        f.write(f"{cat}\n")
+
+# with open(file_2, 'w') as f:
+#     for dog in dogs:
+#         f.write(f"{dog}\n")
+
+files = [file_1, file_2]
+try:
+    for file in files:
+        with open(file) as f:
+            print(f.read())
+except FileNotFoundError:
+    print(f"Sorry, the file {file} could not be found.")
