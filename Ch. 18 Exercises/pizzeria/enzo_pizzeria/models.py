9d17837dc7122bf0c60401fd48b8b56aa7ced7ee
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-06-23 22:06:01 -0400

    Complete Exercise 18-4; Create a new project named Pizzeria with 2 models: Pizza which will have a name field to hold name values, and Topping with a field called 'pizza' that will be a foreign key to Pizza, and 'topping' which will contain name values for toppings.

---
+++ Ch. 18 Exercises/pizzeria/enzo_pizzeria/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.db.models.base import Model
+
+class Pizza(models.Model):
+    """A pizza that the user is building."""
+    name = models.CharField(max_length=150)
+
+    def __str__(self) -> str:
+        """Return the Pizza model name."""
+        return self.name
+
+class Topping(models.Model):
+    """Toppings to be added to pizza."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+
+    class Meta: 
+        verbose_name_plural = "toppings"
+
+    def __str__(self) -> str:
+        return f"_{self.name[:50]}..."
