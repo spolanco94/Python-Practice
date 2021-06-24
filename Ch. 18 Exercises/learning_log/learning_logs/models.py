ba32f0d7bb7f2c7c603c47e62c2c458ce3cb65ce
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-06-23 20:27:08 -0400

    Remove redundant Django projects used for practice, create an Entry model for Learning Log Project that holds a many-to-one relationship with Topic.

--- Ch. 18 Exercises/learning_log/learning_logs/models.py
+++ Ch. 18 Exercises/learning_log/learning_logs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import DateTimeField
 
 class Topic(models.Model):
     """A topic the user is learning about."""
@@ -8,3 +9,18 @@ class Topic(models.Model):
     def __str__(self) -> str:
         """Return string representation of the model."""        
         return self.text
+
+class Entry(models.Model):
+    """An entry specific to a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self) -> str:
+        if(len(self.text) > 50):
+            return f"{self.text[:50]}..."
+        else:
+            return f"{self.text}"
