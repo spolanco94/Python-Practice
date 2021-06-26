8c5b81a7abbe8592ab2e2a3452371a23d230314d
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-06-26 02:35:39 -0400

    Create a page to display all Topics created by users and a page to display details of a Topic, including all its entries.

--- Ch. 18 Exercises/learning_log/learning_logs/urls.py
+++ Ch. 18 Exercises/learning_log/learning_logs/urls.py
@@ -8,4 +8,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that displays topics
+    path('topics/', views.topics, name='topics'),
+    # Detail page for specific topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
