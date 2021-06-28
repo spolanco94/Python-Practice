be93c90d0d0c607e1738ca5c3092e2617e3e9062
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-06-27 22:34:41 -0400

    Create a page that links to and from the home page, displaying a list of all available pizza's.

--- Ch. 18 Exercises/pizzeria/enzo_pizzeria/urls.py
+++ Ch. 18 Exercises/pizzeria/enzo_pizzeria/urls.py
@@ -6,5 +6,10 @@ from . import views
 
 app_name = "enzo_pizzeria"
 urlpatterns = [
+    # Home page
     path('', views.index, name="index"),
+    # Pizza's page
+    path('pizzas/', views.pizzas, name="pizzas"),
+    # Pizza detail page
+    path('pizzas/<str:pizza_name>/', views.pizza, name="pizza")
 ]
