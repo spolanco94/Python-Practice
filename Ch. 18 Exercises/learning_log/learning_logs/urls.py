d6ce84460551c4c8644ad22943e301fe701c81c9
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-07-01 23:07:48 -0400

    Add in a new url path that will direct users to a page to edit existing entries.

--- Ch. 18 Exercises/learning_log/learning_logs/urls.py
+++ Ch. 18 Exercises/learning_log/learning_logs/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
     # Form page to creating a new entry for a specific topic
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Form page for editing an existing entry
+    path('new_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
