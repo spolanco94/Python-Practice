b8b1d51ba61703bc5b0eae6ee6045711769c40a6
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-07-05 19:31:35 -0400

    Create a model for the BlogPost object that will contain information for the blog post's Title, Text, and Date added.

---
+++ Ch. 19 Exercises/Blog/blogs/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.db.models.fields import DateTimeField
+
+class BlogPost(models.Model):
+    """Model for blog post for webapp."""
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        """Return a string representation of the model."""
+        return f"{self.text[:150]}..."
