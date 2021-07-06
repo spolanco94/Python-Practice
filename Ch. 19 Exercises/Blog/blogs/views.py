f3b0e4d0623ad11dbcd583e41fb0f81ebb4693ef
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-07-05 21:58:13 -0400

    Add views function for creating new blog posts.

--- Ch. 19 Exercises/Blog/blogs/views.py
+++ Ch. 19 Exercises/Blog/blogs/views.py
@@ -1,5 +1,5 @@
 import blogs
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import BlogPost
 from .forms import BlogForm
@@ -9,3 +9,19 @@ def index(request):
     blogs = BlogPost.objects.order_by('date_added')
     context = {'blogs': blogs}
     return render(request, 'blogs/index.html', context)
+
+def new_post(request):
+    """Create a new blog post."""
+    if request.method != 'POST':
+        # No form has been submitted, create a new one.
+        form = BlogForm()
+    else:
+        # Data submitted, process and save if valid
+        form = BlogForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogs:index')
+
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'blogs/new_post.html', context)
