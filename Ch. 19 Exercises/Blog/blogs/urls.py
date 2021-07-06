b7719daf1a18acc9bcb29d260fe7ec324e01f25a
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-07-05 21:06:08 -0400

    Add a new URL path to direct users to the post creation form page

--- Ch. 19 Exercises/Blog/blogs/urls.py
+++ Ch. 19 Exercises/Blog/blogs/urls.py
@@ -9,4 +9,6 @@ app_name = 'blogs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Create a new blog post
+    path('new_post', views.new_post, name='new_post'),
 ]
