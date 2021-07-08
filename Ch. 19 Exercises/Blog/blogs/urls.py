e3f00b5305816c3220372be7912aee7ecbc63613
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-07-07 22:39:24 -0400

    Add a new path to a page where users can edit individual  blog posts.

--- Ch. 19 Exercises/Blog/blogs/urls.py
+++ Ch. 19 Exercises/Blog/blogs/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Create a new blog post
     path('new_post', views.new_post, name='new_post'),
+    # Edit blog posts
+    path('edit_post/<int:blog_id>', views.edit_post, name='edit_post'),
 ]
